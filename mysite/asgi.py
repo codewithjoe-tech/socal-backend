aaed7853776d640afac470e2de848cfe49e58f9d
Joel Thomas <joel@codewithjoe.in>
2024-11-14 01:19:55 +0530

    error solved

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -1,15 +1,17 @@
-
-
 # ASGI configuration
 import os
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
-from Chat.routings import websocket_urlpatterns
 from .channelsmiddleware import JWTAuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+django.setup()
+
+from Chat.routings import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
