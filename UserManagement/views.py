a896ceb603fca29d87267ea74b9e25c8d4969469
Joel Thomas <joel@codewithjoe.in>
2024-11-17 09:08:04 +0530

    hello

--- UserManagement/views.py
+++ UserManagement/views.py
@@ -90,7 +90,7 @@ class GoogleCallback(PublicApi):
             "grant_type":"authorization_code"
         }
         token_response = requests.post(token_url,data=token_data)
-        print(token_response)
+        print(token_response.json())
         token_json = token_response.json()
         access_token = token_json.get('access_token')
 
