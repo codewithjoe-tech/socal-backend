a1c520598ac61cf1a0a15b128a92fcb40f68ca8c
Joel Thomas <joel@codewithjoe.in>
2024-11-17 10:46:24 +0530

    increased space for posting

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,7 +76,11 @@ LOGGING = {
     },
 }
 
-DATA_UPLOAD_MAX_NUMBER_FIELDS = 7000
+
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10 MB
+FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10 MB
+
+CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = ['http://localhost:5173']
 DATASET_DIR = os.path.join(BASE_DIR, 'data')
@@ -86,6 +90,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Add the URL of your frontend
     "https://friendbook.api.codewithjoe.in",  # Optional: add production URL if needed
 ]
+SECURE_SSL_REDIRECT = True
+
 AUTH_USER_MODEL = 'UserManagement.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
