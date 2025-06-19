3e2c1621fb712c69b0dc63b71e34f7a52e37ac8c
Joel Thomas <joel@codewithjoe.in>
2025-06-19 15:40:31 +0530

    new commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,8 +114,12 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),  # e.g. db or localhost
+        'PORT': '5432',
     }
 }
 
